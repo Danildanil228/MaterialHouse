042f6393bc202bc2ebeb193029becd9d3a392eff
Danil <d_silchenkov@mail.ru>
2025-12-23 19:12:34 +0200

    first commit

---
+++ front/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { ThemeProvider } from "@/components/theme-provider"
+import Login from './Pages/Login'
+import DarkModeButtonToggle from './components/DarkModeButtonToggle'
+
+function App() {
+
+  return (
+    <>
+      <Login/>
+      <DarkModeButtonToggle/>
+    </>
+  )
+}
+
+export default App
